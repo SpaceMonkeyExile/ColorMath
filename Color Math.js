3c832aaf830b1c9817ed72c69619cb444ec90b95
Brian S <Schmidty.brian@yahoo.com>
2023-12-25 20:35:01 -0600

    Add files via upload

---
+++ Color Math.js
@@ -0,0 +1,200 @@
+/*
+Auth:  Brian Schmidt
+Date:  12/26/23
+Desc:  Color Math 2
+    Color Math will hopefully be a way to make math easier to learn by assigning
+a set color to each digit 0-9.  the idea is to add a second level to the way we 
+precieve numbers to hopefully help people learn math easier.
+    Technology has come to a point where I think it is possible to add this to learning
+where before it would have been wildly impractical.  It will still be difficult to 
+have written maths but possibly with tablet writers that can be fixed.
+*/
+
+// set basic variables
+var Xposition = 20;
+var Yposition = 50;
+
+// text size
+textSize (60);
+
+
+// black chalk board cannot use black
+background(0, 0, 0);
+
+// Yellow and number 1
+fill(255, 255, 0);
+text("1", Xposition,Yposition);
+
+// Orange and number 2
+fill(255, 128, 0);
+text("2", Xposition+35,Yposition);
+
+// Red and number 3
+fill(255, 0, 0);
+text("3", Xposition+70,Yposition);
+
+// Purple and number 4
+fill(128, 0, 128);
+text("4", Xposition+105,Yposition);
+
+// Blue and number 5
+fill(0, 0, 255);
+text("5", Xposition+140,Yposition);
+
+// Green and number 6
+fill(0, 128, 0);
+text("6", Xposition+175,Yposition);
+
+// lt. Blue and number 7
+fill(0, 255, 255);
+text("7", Xposition+210,Yposition);
+
+// Pink and number 8
+fill(255, 0, 128);
+text("8", Xposition+245,Yposition);
+
+// Grey and number 9
+fill(128, 128, 128);
+text("9", Xposition+280,Yposition);
+
+// white and number 0
+fill(255, 255, 255);
+text("0", Xposition+315,Yposition);
+
+/* This is
+a line break */
+
+//middle grey chalk board
+fill(120, 120, 120);
+rect(0, 80, 400, 100);
+
+// White and number 1
+fill(255, 255, 255);
+text("1", Xposition,Yposition+100);
+
+// Yellow and number 2
+fill(255, 255, 0);
+text("2", Xposition+35,Yposition+100);
+
+// Orange and number 3
+fill(255, 128, 0);
+text("3", Xposition+70,Yposition+100);
+
+// Red and number 4
+fill(255, 0, 0);
+text("4", Xposition+105,Yposition+100);
+
+// Purple and number 5
+fill(255, 0, 255);
+text("5", Xposition+140,Yposition+100);
+
+// blue and number 6
+fill(0, 0, 255);
+text("6", Xposition+175,Yposition+100);
+
+// green and number 7
+fill(0, 225, 0);
+text("7", Xposition+210,Yposition+100);
+
+// Pink and number 8
+fill(255, 0, 128);
+text("8", Xposition+245,Yposition+100);
+
+// Teal and number 9
+fill(0, 255, 255);
+text("9", Xposition+280,Yposition+100);
+
+// black and number 0
+fill(0, 0, 0);
+text("0", Xposition+315,Yposition+100);
+
+/* This is
+a line break */
+
+fill(255, 255, 255);
+rect(0, 180, 400, 100);
+
+// Yellow and number 1
+fill(255, 255, 0);
+text("1", Xposition,Yposition+200);
+
+// Orange and number 2
+fill(255, 128, 0);
+text("2", Xposition+35,Yposition+200);
+
+// Red and number 3
+fill(255, 0, 0);
+text("3", Xposition+70,Yposition+200);
+
+// Purple and number 4
+fill(128, 0, 128);
+text("4", Xposition+105,Yposition+200);
+
+// Blue and number 5
+fill(0, 0, 255);
+text("5", Xposition+140,Yposition+200);
+
+// Green and number 6
+fill(0, 128, 0);
+text("6", Xposition+175,Yposition+200);
+
+// lt. Blue and number 7
+fill(0, 255, 255);
+text("7", Xposition+210,Yposition+200);
+
+// Pink and number 8
+fill(255, 0, 128);
+text("8", Xposition+245,Yposition+200);
+
+// Grey and number 9
+fill(128, 128, 128);
+text("9", Xposition+280,Yposition+200);
+
+// Black and number 0
+fill(0, 0, 0);
+text("0", Xposition+315,Yposition+200);
+
+
+// bottom half green chalk board
+fill(0, 85, 0);
+rect(0, 280, 400, 100);
+
+//  black number 0
+fill(0, 0, 0);
+text("0", Xposition,Yposition+300);
+
+//  white number 1
+fill(255, 255, 255);
+text("1", Xposition+35,Yposition+300);
+
+//  yellow 2
+fill(255, 255, 0);
+text("2", Xposition+70,Yposition+300);
+
+//  orange 3
+fill(255, 128, 0);
+text("3", Xposition+105,Yposition+300);
+
+//  red number 4
+fill(255, 0, 0);
+text("4", Xposition+140,Yposition+300);
+
+//  grey number 5
+fill(128, 128, 128);
+text("5", Xposition+175,Yposition+300);
+
+//  purple number 6
+fill(128, 0, 255);
+text("6", Xposition+210,Yposition+300);
+
+//  blue number 7
+fill(0, 0, 255);
+text("7", Xposition+245,Yposition+300);
+
+// green number 8
+fill(0, 255, 0);
+text("8", Xposition+280,Yposition+300);
+
+// teal and number 9
+fill(0, 255, 255);
+text("9", Xposition+315,Yposition+300);
